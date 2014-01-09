fc56c7521c695aca7aa5f94aed201dd75c039e37
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-09 20:56:08 +0000

    Added tests

--- tests/spec.js
+++ tests/spec.js
@@ -1,33 +1,88 @@
 var should = require('should'),
-    myriad = require('./../module/Myriad.js');
+    _      = require('underscore'),
+    Myriad = require('./../module/Myriad.js');
 
 describe('Snapshot.js', function() {
 
-    var $myriadDefault, $myriadCustom;
+    describe('Default Params', function() {
 
-    beforeEach(function() {
-        
-        var object = {
-            myriad: function(properties) {
+        var $myriad;
 
-            }
-        };
-
-        var model = { name: null, age: null, location: null };
-
-        $myriadDefault = new Myriad({
-            model:  model,
-            invoke: object.myriad,
-            prefix: 'getBy',
-            levels: 3
+        beforeEach(function() {
+            $myriad     = new Myriad({
+                model:  { name: null, age: null, location: null },
+                invoke: function() {},
+                prefix: 'getBy'
+            });
         });
-        
+
+        it('Can setup basic functions with default params', function() {
+            $myriad.getByNameAndAge.should.be.an.instanceOf(Function);
+            $myriad.getByAgeAndName.should.be.an.instanceOf(Function);
+            $myriad.getByAgeAndNameAndLocation.should.be.an.instanceOf(Function);
+        });
+
     });
 
-    describe('Basic', function() {
+    describe('Custom Level', function() {
 
-        it('Can setup basic functions', function() {
-            expect(typeof $myriadDefault.getByNameAndId).toEqual('function');
+        var $myriad;
+
+        beforeEach(function() {
+            $myriad     = new Myriad({
+                model:  { name: null, age: null, location: null },
+                invoke: function() {},
+                prefix: 'getBy',
+                levels: 2
+            });
+        });
+
+        it('Can setup basic functions with default params', function() {
+            $myriad.getByLocation.should.be.an.instanceOf(Function);
+            $myriad.getByAgeAndLocation.should.be.an.instanceOf(Function);
+            should.strictEqual(undefined, $myriad.getByAgeAndNameAndLocation);
+        });
+
+    });
+
+    describe('Custom Prefix', function() {
+
+        var $myriad;
+
+        beforeEach(function() {
+            $myriad     = new Myriad({
+                model:  { name: null, age: null, location: null },
+                invoke: function() {},
+                prefix: 'fetch'
+            });
+        });
+
+        it('Can setup basic functions with default params', function() {
+            should.strictEqual(undefined, $myriad.getByAge);
+            should.strictEqual(undefined, $myriad.getByAgeAndNameAndLocation);
+            $myriad.fetchAge.should.be.an.instanceOf(Function);
+            $myriad.fetchNameAndLocation.should.be.an.instanceOf(Function);
+        });
+
+    });
+
+    describe('Custom Blacklist', function() {
+
+        var $myriad;
+
+        beforeEach(function() {
+            $myriad     = new Myriad({
+                model:  { name: null, age: 12, location: null },
+                invoke: function() {},
+                blacklist: ['number']
+            });
+        });
+
+        it('Can setup basic functions with default params', function() {
+            should.strictEqual(undefined, $myriad.getByAge);
+            should.strictEqual(undefined, $myriad.getByAgeAndLocationAndName);
+            $myriad.getByNameAndLocation.should.be.an.instanceOf(Function);
+            $myriad.getByLocationAndName.should.be.an.instanceOf(Function);
         });
 
     });
